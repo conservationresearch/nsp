e99ea28775c4b8ddf924b50e45944c9e83b15cd7
Laura Keating <LauraK@calgaryzoo.com>
2022-11-17 14:32:46 -0700

    Changed example again, exploring combos to see which is a nice set to show someone

--- Scripts/example_data_setup.R
+++ Scripts/example_data_setup.R
@@ -9,23 +9,35 @@ inputs<-read.csv(file="C:/Users/LauraK/OneDrive - The Calgary Zoological Society
 # Choose a few species to anonymize and include, include 'NA'
 species <- unique(inputs$species)
 
-species_1 <- species[3]# ferret
-species_2 <- species[5] # lichen
-species_3 <- species[8] # skipper
-species_4 <- species[14] # frog
-# species[13], species[16], species[19]
+# species_1 <- species[3]# ferret
+# species_2 <- species[5] # lichen
+# species_3 <- species[8] # skipper
+# species_4 <- species[14] # frog
+# # species[13], species[16], species[19]
+
+species_1 <- species[2]# snail
+species_2 <- species[8] # skipper
+species_3 <- species[11] # hairstreak
+species_4 <- species[13] # ringlet
+species_5 <- species[17] # checkerspot
+species_6 <- species[20] # yucca
+
 
 rows_to_include_generic <- which(inputs$species == "N/A")
 rows_to_include_species_1 <- which(inputs$species == species_1)
 rows_to_include_species_2 <- which(inputs$species == species_2)
 rows_to_include_species_3 <- which(inputs$species == species_3)
 rows_to_include_species_4 <- which(inputs$species == species_4)
+rows_to_include_species_5 <- which(inputs$species == species_5)
+rows_to_include_species_6 <- which(inputs$species == species_6)
 
 rows_to_include <- c(rows_to_include_generic,
                      rows_to_include_species_1,
                      rows_to_include_species_2,
                      rows_to_include_species_3,
-                     rows_to_include_species_4)
+                     rows_to_include_species_4,
+                     rows_to_include_species_5,
+                     rows_to_include_species_6)
 
 dat_example <- inputs[rows_to_include,]
 
@@ -34,11 +46,15 @@ dat_example$species[which(dat_example$species == species_1)] <- "Species 1"
 dat_example$species[which(dat_example$species == species_2)] <- "Species 2"
 dat_example$species[which(dat_example$species == species_3)] <- "Species 3"
 dat_example$species[which(dat_example$species == species_4)] <- "Species 4"
+dat_example$species[which(dat_example$species == species_5)] <- "Species 5"
+dat_example$species[which(dat_example$species == species_6)] <- "Species 6"
 
 dat_example$name[which(dat_example$species == "Species 1")] <- sub(paste0(species_1,"_"),"Species 1_", dat_example$name[which(dat_example$species == "Species 1")])
 dat_example$name[which(dat_example$species == "Species 2")] <- sub(paste0(species_2,"_"),"Species 2_", dat_example$name[which(dat_example$species == "Species 2")])
 dat_example$name[which(dat_example$species == "Species 3")] <- sub(paste0(species_3,"_"),"Species 3_", dat_example$name[which(dat_example$species == "Species 3")])
 dat_example$name[which(dat_example$species == "Species 4")] <- sub(paste0(species_4,"_"),"Species 4_", dat_example$name[which(dat_example$species == "Species 4")])
+dat_example$name[which(dat_example$species == "Species 5")] <- sub(paste0(species_5,"_"),"Species 5_", dat_example$name[which(dat_example$species == "Species 5")])
+dat_example$name[which(dat_example$species == "Species 6")] <- sub(paste0(species_6,"_"),"Species 6_", dat_example$name[which(dat_example$species == "Species 6")])
 
 # call it the same thing as before for simplicity
 ExampleDataset <- dat_example
